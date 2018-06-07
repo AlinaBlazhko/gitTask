321320aaf64f0f37b6c99cec5b274ce31ca84bb2
Alina_Blazhko <alinablazhko420@gmail.com>
2018-06-07 14:41:11 +0300

    Update Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         CalculatorUtil calculator = new CalculatorUtil();
         System.out.println(calculator.calculation(2, 3, "+"));
+
+
     }
 }
