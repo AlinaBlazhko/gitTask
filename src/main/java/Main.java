a842d6909eb993157061c32a0f68d3c70aa9fa31
Alina_Blazhko <alinablazhko420@gmail.com>
2018-06-07 14:36:39 +0300

    Rewrite Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CalculatorUtil calculator = new CalculatorUtil();
+        System.out.println(calculator.calculation(2, 3, "+"));
     }
 }
