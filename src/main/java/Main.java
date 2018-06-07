339cadd13bf6b9fb7cb5679be0c82c7a98a04cdf
Alina_Blazhko <alinablazhko420@gmail.com>
2018-06-07 14:50:23 +0300

    add assertion for calculator answer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ public class Main {
         CalculatorUtil calculator = new CalculatorUtil();
         System.out.println(calculator.calculation(2, 3, "+"));
 
-
+        if (calculator.calculation(2, 3, "+") == 5) {
+            System.out.println("Correct answer:)");
+        }else {
+            System.out.println("Fail:(");
+        }
     }
 }
